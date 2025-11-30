e348aa4d9373fc380ec1801bdf095560cca2ff80
Xavier <xaviersignup@gmail.com>
2025-11-30 11:20:07 -0800

    Started on files for support on esp32

---
+++ include/QSerialConnection.h
@@ -0,0 +1,11 @@
+#ifndef QSERIALCONNECTION_H
+#define QSERIALCONNECTION_H
+
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+
+class QSerialConnection {
+
+};
+
+#endif // QSERIALCONNECTION_H
