c5cb55abcb8c7abb09e2f6e2ef82f7bcfa55b834
Xavier <xaviersignup@gmail.com>
2025-11-30 11:44:46 -0800

    Progress on QSerialConnection header file

--- include/QSerialConnection.h
+++ include/QSerialConnection.h
@@ -1,11 +1,29 @@
 #ifndef QSERIALCONNECTION_H
 #define QSERIALCONNECTION_H
 
+#include <QObject>
 #include <QtSerialPort/QSerialPort>
 #include <QtSerialPort/QSerialPortInfo>
 
-class QSerialConnection {
 
+class QSerialConnection : public QObject {
+    Q_OBJECT
+
+public:
+    explicit QSerialConnection(QObject *parent = nullptr);
+    ~QSerialConnection();
+
+    bool openPort(const QString &portName, int baudRate = QSerialPort::Baud115200);
+    void closePort();
+    bool isOpen() const;
+
+    QList<QSerialPortInfo> availablePorts() const;
+    bool writeData(const QByteArray &data);
+
+signals:
+
+
+private:
 };
 
 #endif // QSERIALCONNECTION_H
